b209b55c136a991dda16e469216bb66a1da5de48
Greg Almeida <greg.almeidasilva@gmail.com>
2022-08-24 07:10:00 -0300

    chore: update for course delivery

--- techtrends/app.py
+++ techtrends/app.py
@@ -5,11 +5,17 @@ import sys
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
+
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
+
     return connection
 
 # Function to get a post using its ID
@@ -97,7 +103,7 @@ def metrics():
 
 
     response = app.response_class(
-        response=json.dumps({"db_connection_count": 1,"post_count": post_count}),
+        response=json.dumps({"db_connection_count": db_connection_count,"post_count": post_count}),
         status=200,
         mimetype='application/json'
     )
